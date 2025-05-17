4b02ec6ec52b670fd03701ed3b4cf30de33bfe05
ArkoSammy12 <121594394+ArkoSammy12@users.noreply.github.com>
2025-05-16 23:59:47 -0500

    Use HttpURLConnection from java.net instead of HttpsURLConnection to support VelocityProxyAuthHook authentication (#143)

--- src/main/java/com/legacyminecraft/poseidon/util/SessionAPI.java
+++ src/main/java/com/legacyminecraft/poseidon/util/SessionAPI.java
@@ -3,9 +3,9 @@ package com.legacyminecraft.poseidon.util;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
-import javax.net.ssl.HttpsURLConnection;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 /**
@@ -57,7 +57,7 @@ public class SessionAPI
         try
         {
             URL obj = new URL(url);
-            HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+            HttpURLConnection con = (HttpURLConnection) obj.openConnection();
             con.setRequestMethod("GET");
             con.setRequestProperty("User-Agent", "Project-Poseidon/0");
             BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
